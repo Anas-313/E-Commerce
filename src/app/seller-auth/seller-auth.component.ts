da1fe322268ae307dfa5505e9397fce565e6bada
Anas-313 <manaspatrawala313@gmail.com>
2023-03-26 09:13:58 +0530

    initial Commit

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { SellerService } from '../Services/seller.service';
+import { Router } from '@angular/router';
+import { signUp } from '../data-type';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent {
+  constructor(private seller:SellerService, private router:Router) {
+
+  }
+  signUp(data:signUp):void {
+
+    this.seller.userSignUp(data).subscribe((result)=>{
+      console.warn(result)
+      if(result) {
+          this.router.navigate(['seller-home'])
+      }
+    });
+  }
+}
